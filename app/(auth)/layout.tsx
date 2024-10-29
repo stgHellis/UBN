95f6e628f8726af89b43668082ee60c2922f5cee
stgHellis <etienneake@hotmail.fr>
2024-10-29 20:59:23 +0100

    Reconnection à Github + 4 changements

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
           <Image
             src="/icons/ubnbank.png"
             alt="Auth image"
-            width={500}
-            height={500}
+            width={650}
+            height={680}
             className="rounded-l-xl object-contain"
           />
         </div>
